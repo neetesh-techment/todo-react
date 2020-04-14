266eb498ef9f9e5a61a678b932810ebb56c7c19c
Neetesh Gupta <neetesh.g@techment.com>
2020-04-14 16:50:49 +0530

    input and header added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      noteText: ''
+    }
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <div className="header">React TODO Application</div>
+        <div className="btn">+</div>
+        <input type="text" ref={((input) => {this.textInput = input})} />
+      </div>
+    );
+  }
 }
 
 export default App;
