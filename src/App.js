8712f2db583bf9900b46d9a19fca20b65b99c0e3
Neetesh Gupta <neetesh.g@techment.com>
2020-04-15 17:34:10 +0530

    todo application complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Note from './components/Note';
 import './App.css';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      noteText: ''
+      noteText: '',
+      notes: []
     }
   }
 
@@ -13,16 +15,44 @@ class App extends React.Component {
     this.setState({noteText: noteText.target.value})
   }
 
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      let notesArr = this.state.notes;
+      notesArr.push(this.state.noteText);
+      this.setState({noteText: ''})
+    }
+  }
+
+  deleteNote(index){
+    let notesArr = this.state.notes;
+    notesArr.slice(index, 1);
+    this.setState({notes: notesArr})
+  }
+
+  addNote(){
+    if(this.state.noteText === ''){return}
+    let notesArr = this.state.notes;
+    notesArr.push(this.state.noteText);
+    this.setState({noteText: ''})
+    this.textInput.focus();
+  }
+
   render(){
+    let notes = this.state.notes.map((key, value) => {
+      return <Note key={key} text={key} deleteMethod={() => this.deleteNote(key)}/>
+    })
+
     return (
       <div className="container">
         <div className="header">React TODO Application</div>
-        <div className="btn">+</div>
+        {notes}
+        <div className="btn" onClick={this.addNote.bind(this)}>+</div>
         <input type="text"
                ref={((input) => {this.textInput = input})}
                className="textInput"
                value={this.state.noteText}
-               onChangeText={noteText => this.updateNoteText(noteText)} />
+               onChange={noteText => this.updateNoteText(noteText)}
+               onPressKey={this.handleKeyPress.bind(this)}/>
       </div>
     );
   }
